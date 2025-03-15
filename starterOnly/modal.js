3089b67145bfd334dc6b018a0095fd1747841425
Vassili <vassililanglois@icloud.com>
2025-03-15 18:52:31 +0100

    CSS style and responsive modifications on modal

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -7,6 +7,15 @@ function editNav() {
   }
 }
 
+function editNav2() {
+  var x = document.getElementById("myTopnav2");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+
 // DOM Elements
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
